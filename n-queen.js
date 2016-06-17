6c2f61f648cff1a4b579952d38e9756aa79ecd93
mredmundto <yatlunto@gmail.com>
2016-06-16 18:19:42 -0700

    helper function column major diagonal, yet to do minor diagonal

--- n-queen.js
+++ n-queen.js
@@ -1,6 +1,10 @@
 var table1 = [[0,0,1,0],[1,0,0,0],[0,0,0,1],[0,1,0,0]] // Good case
 var table2 = [[1,0,0,0],[1,0,0,0],[1,0,0,0],[1,0,0,0]] // Columns 
-var table3 = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]] // Diagonal 
+var table3 = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]] // Major Diagonal 
+var table4 = [[0,0,1,0],[0,0,0,1],[0,0,0,0],[0,0,0,0]] // Small Major Diagonal 
+var table5 = [[0,0,0,1],[0,0,1,0],[0,1,0,0],[1,0,0,0]] // Minor Diagonal 
+var table6 = [[0,0,0,0],[0,0,0,0],[1,0,0,0],[0,1,0,0]] // Small Minor Diagonal 
+
 
 Array.prototype.checkColumn = function(){
 	var count = 0; 
@@ -20,26 +24,40 @@ Array.prototype.checkColumn = function(){
 Array.prototype.checkDiagonal = function(){
 
 	// check Major Diagonal 
-	count = 0; 
-	for (var i = 0; i < this.length; i++){
-		count += this[i][i]
-		if (count > 1){
-			return false
+	var count = 0;
+	var len = this.length; 
+	
+	for (var j = -len; j < len; j++){
+		for (var i = 0; i < len; i++){
+			if (this[i][i+j] !== undefined){
+				count += this[i][i+j];
+				if (count > 1){
+					return false; 
+				}
+			}	
 		}
+		count = 0; 
 	}
-	return true; 
+		
 	// check Minor Diagonal 
+	return true; 
+	
 }
 
-
-
-
+console.log("Good case")
 console.log(table1.checkColumn())
 console.log(table1.checkDiagonal())
 
+console.log("Column conflict")
 console.log(table2.checkColumn())
 console.log(table2.checkDiagonal())
 
+console.log("Major Diagonal conflict")
 console.log(table3.checkColumn())
 console.log(table3.checkDiagonal())
+
+console.log("Small Major Diagonal conflict")
+console.log(table4.checkColumn()) 
+console.log(table4.checkDiagonal())
+
 
